b72cd5562c182b46586dc63285e820ac44ed7a30
zzq <2710790035@qq.com>
2018-11-14 09:26:44 +0800

    添加方法

--- git/src/com/itcast/git.java
+++ git/src/com/itcast/git.java
@@ -11,12 +11,13 @@ public class git {
         System.out.println("hello");
         System.out.println("hello");
 
-
-
     }
     public void eat(){
-
         System.out.println("吃");
         System.out.println("hehehe");
+    }
+    public void sleep(){
+        System.out.println("睡");
+        System.out.println("hello");
     }
 }
